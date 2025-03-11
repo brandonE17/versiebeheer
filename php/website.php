4b9380ecb65892b3d02a4ebdbc18b27f1e677df6
brandonE17 <6044720@mborijnland.nl>
2025-03-11 09:30:29 +0100

    de genre sectie toegevoegd

--- php/website.php
+++ php/website.php
@@ -1,1 +1,21 @@
 
+<?php
+$games = [
+    [
+        "name" => "Game 1",
+        "image" => "images/game1.png"
+    ],
+    [
+        "name" => "Game 2",
+        "image" => "images/game2.png"
+    ],
+    [
+        "name" => "Game 3",
+        "image" => "images/game3.png"
+    ]
+];
+
+header("Content-Type: application/json");
+echo json_encode($games);
+?>
+  
